b90c7db6ca4ebf66cc9dc7baf34da686ad458036
Pranav Shrivastav <rishishrivastav2211@gmail.com>
2025-03-07 20:39:45 +0530

    [Pranav] Add. Restrict Alphabets

--- PINCode/PINCodeValidation.java
+++ PINCode/PINCodeValidation.java
@@ -4,10 +4,17 @@ public class PINCodeValidation {
     public static void main(String[] args) {
         // Test PIN code
         String pinCode = "400088";
+        
+        String pinCode2 = "A400088";
+
         // PIN code validation pattern
         String regex = "^[1-9][0-9]{2}\\s?[0-9]{3}$";
 
         boolean isValid = Pattern.matches(regex, pinCode);
         System.out.println("Is valid PIN Code? " + isValid);
+
+        // It should fail
+        boolean isValid2 = Pattern.matches(regex, pinCode2);
+        System.out.println("Is valid PIN Code? " + isValid2);
     }
 }
